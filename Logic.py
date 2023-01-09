bfdb0c2b167036e1a88432cab880d3399cffd625
Gedasikas <120640245+Gedasikas@users.noreply.github.com>
2023-01-09 14:25:05 +0200

    Minimali logika

--- Logic.py
+++ Logic.py
@@ -1,62 +1,36 @@
-stages = [
-        '''
-        ____
-        |   |
-        |   0
-        |  /|\\
-        |  / \\
-       _|_______
-        ''',
-        '''
-        ____
-        |   |
-        |   0
-        |  /|\\
-        |  / 
-       _|_______
-        ''',
-        '''
-        ____
-        |   |
-        |   0
-        |  /|\\
-        |  
-       _|_______
-        ''',
-        '''
-        ____
-        |   |
-        |   0
-        |  /|
-        |  
-       _|_______
-        ''',
-        '''
-        ____
-        |   |
-        |   0
-        |   |
-        |  
-       _|_______
-        ''',
-        '''
-        ____
-        |   |
-        |   0
-        |  
-        |  
-       _|_______
-        ''',
-        '''
-        ____
-        |   |
-        |   
-        |  
-        |  
-       _|_______
-        '''
-    ][::-1]
-try:
-    ivesta_raide  = input("Įvesk raidę: ")
-except:
+from Hangman_Images import stages
+
+
+zodziai = "gedas"
+gyvybes = 0
+reikiamos_raides = []
+buvusios_raides = []
+for raide in zodziai:
+    reikiamos_raides.append(raide)
+
+while True:
+    if gyvybes == 7:
+        print(stages[-1])
+        print(f"###PRALAIMĖJAI###\nŽodis buvo: {zodziai}")
+        break
+    else:
+        ivesta_raide = input("Įvesk raidę: ").lower()
+        if ivesta_raide in reikiamos_raides and ivesta_raide not in buvusios_raides:
+            buvusios_raides.append(ivesta_raide)
+            print("Teisingai!")
+            print(f"Spėtos raidės: {buvusios_raides}")
+
+        elif ivesta_raide not in reikiamos_raides:
+            buvusios_raides.append(ivesta_raide)
+            gyvybes += 1
+            print(f"Neteisingai")
+            print(f"Spėtos raidės: {buvusios_raides}")
+            print(f"{stages[gyvybes]}")
+
+        elif ivesta_raide in buvusios_raides:
+            print("Šitą raidę jau spėjai!")
+            print(f"Spėtos raidės: {buvusios_raides}")
+
+
+
 
